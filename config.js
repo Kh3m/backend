51ff612661e81c0229eeca6a49a81dd905bc9299
Chinonso Obidike <gloryshines4ever@yahoo.com>
2020-10-15 17:26:21 -0400

    added mongodb, routes and model for users

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 export default {
-    TRAVELPAYOUT_API: process.env.REACT_APP_TRAVELPAYOUTS_API
+    TRAVELPAYOUTS: process.env.REACT_APP_TRAVELPAYOUTS_API,
+    RAPIDAPI: process.env.RAPID_API,
+    MONGODB: process.env.MONGODB_URL || 'mongodb://localhost/flightapp',
+    TP_URL: process.env.TP_URL,
+    JWT_SECRET: process.env.JWT_SECRET
 }
--- server.js
+++ server.js
@@ -1,38 +1,56 @@
 import express from 'express';
 import data from './data.js';
-const axios = require('axios')
-require('dotenv').config()
+import config from './config'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import userRoute from './routes/userRoute'
+import axios from 'axios'
+// const axios = require('axios')
+
+// require('dotenv').config()
 // const fetch = require('node-fetch');
 const app = express();
-const tp_token = process.env.REACT_APP_TRAVELPAYOUTS_API
-const rapid_api = process.env.REACT_APP_RAPID_API
+dotenv.config()
 
+const mongodburl = config.MONGODB
+const tp_token = config.TRAVELPAYOUTS
+const rapid_api = config.RAPIDAPI
+const tp_url = config.TP_URL
 
+mongoose.connect(mongodburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason))
+// const tp_token = process.env.REACT_APP_TRAVELPAYOUTS_API
+// const rapid_api = process.env.REACT_APP_RAPID_API
+
+app.use("/api/users", userRoute)
 app.get("/api/cities", (req, res) => {
     res.send(data.cities);
 });
 app.get("/api/airlines", (req, res) => {
     res.send(data.airlines);
 });
-
 app.get("/api/flights", (req, res) => {
     let origin = req.query.origin
     let dest = req.query.destination
     let departure = req.departureDate
     let arrival = req.returnDate
-    let url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/"
-    // let testurl = "https://test.com"
+    // let url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/"
+    let url = "https://test.com"
 
     axios({
         "method": "GET",
         "url": url,
-        "headers": {
-            "content-type": "application/octet-stream",
-            "x-access-token": tp_token,
-            "x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-            "x-rapidapi-key": rapid_api,
-            "useQueryString": true
-        }, "params": {
+        // "headers": {
+        //     "content-type": "application/octet-stream",
+        //     "x-access-token": tp_token,
+        //     "x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+        //     "x-rapidapi-key": rapid_api,
+        //     "useQueryString": true
+        // }, 
+        "params": {
             "destination": dest,
             "origin": origin,
             "depart_date": departure,
@@ -47,6 +65,7 @@ app.get("/api/flights", (req, res) => {
             console.log(error)
         })
 })
+
 
 
 app.listen(5000, () => console.log("Server started at http://localhost:5000"));
